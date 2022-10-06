643a448776e03bc9cb8a2545d92207e165223447
KobayashiYoh <y.kobayashi@4nonome.com>
2022-10-06 20:44:58 +0900

    Formを追加

--- lib/function_20221006/weather_forecasts_form_page.dart
+++ lib/function_20221006/weather_forecasts_form_page.dart
@@ -9,7 +9,10 @@ class WeatherForecastsFormPage extends StatefulWidget {
 }
 
 class _WeatherForecastsFormPageState extends State<WeatherForecastsFormPage> {
+  final _formKey = GlobalKey<FormState>();
   TextEditingController _controller = TextEditingController();
+  final String message = '地域ID（6ケタの数字）を入力してください。';
+  final String pattern = r'^[0-9]{6}$';
 
   void _clearText() {
     setState(() {
@@ -17,42 +20,60 @@ class _WeatherForecastsFormPageState extends State<WeatherForecastsFormPage> {
     });
   }
 
+  void submit() {
+    if (_formKey.currentState!.validate()) {
+      print('success');
+    } else {
+      print('faild');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Container(
-          padding: const EdgeInsets.symmetric(vertical: 64.0, horizontal: 16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const Expanded(
-                child: Center(
-                  child: Text('結果を表示'),
+    return Form(
+      key: _formKey,
+      child: Scaffold(
+        body: SafeArea(
+          child: Container(
+            padding:
+                const EdgeInsets.symmetric(vertical: 64.0, horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const Expanded(
+                  child: Center(
+                    child: Text('結果を表示'),
+                  ),
                 ),
-              ),
-              const Text('地域ID（6ケタの数字）を入力してください。'),
-              TextFormField(
-                controller: _controller,
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: TextButton(
-                      onPressed: _clearText,
-                      child: const Text('クリア'),
+                Text(message),
+                TextFormField(
+                  controller: _controller,
+                  validator: (value) {
+                    if (!RegExp(pattern).hasMatch(value!)) {
+                      return message;
+                    }
+                    return null;
+                  },
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: TextButton(
+                        onPressed: _clearText,
+                        child: const Text('クリア'),
+                      ),
                     ),
-                  ),
-                  const SizedBox(width: 16.0),
-                  Expanded(
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text('送信'),
+                    const SizedBox(width: 16.0),
+                    Expanded(
+                      child: TextButton(
+                        onPressed: submit,
+                        child: const Text('送信'),
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
