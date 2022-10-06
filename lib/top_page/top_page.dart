3ca482b799c10787b6f8ab90d0bf448a8660621e
KobayashiYoh <y.kobayashi@4nonome.com>
2022-10-06 18:21:18 +0900

    ItemListViewクラスを作成

--- lib/top_page/top_page.dart
+++ lib/top_page/top_page.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:weekly_trace_challenge/top_page/item_list_view.dart';
 
 class TopPage extends StatelessWidget {
   const TopPage({Key? key}) : super(key: key);
-
-  final List<Widget> _tabs = const [
-    Tab(text: 'UI Trace'),
-    Tab(text: 'Function Trace'),
+  final List<String> _titleTexts = const [
+    'UI Trace',
+    'Function Trace',
   ];
 
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       initialIndex: 1,
-      length: _tabs.length,
+      length: _titleTexts.length,
       child: Scaffold(
         appBar: AppBar(
           title: const Text('Weekly Trace Challenge'),
           bottom: TabBar(
-            tabs: _tabs,
+            tabs: <Widget>[
+              for (var text in _titleTexts) Tab(text: text),
+            ],
           ),
         ),
         body: TabBarView(
           children: <Widget>[
-            for (int i = 0; i < _tabs.length; i++)
-              Center(
-                child: _tabs[i],
+            for (int i = 0; i < _titleTexts.length; i++)
+              ItemListView(
+                index: i,
               ),
           ],
         ),
