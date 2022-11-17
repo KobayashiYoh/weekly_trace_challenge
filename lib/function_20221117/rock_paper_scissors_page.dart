313fe28b04dc4eb902c3cded7e088dab11039897
KobayashiYoh <y.kobayashi@4nonome.com>
2022-11-17 14:21:44 +0900

    じゃんけんのロジックを作成

--- lib/function_20221117/rock_paper_scissors_page.dart
+++ lib/function_20221117/rock_paper_scissors_page.dart
@@ -1,20 +1,77 @@
 import 'package:flutter/material.dart';
 
-class RockPaperScissorsPage extends StatelessWidget {
+enum HandSelect {
+  rock,
+  paper,
+  scissors,
+}
+
+class RockPaperScissorsPage extends StatefulWidget {
   const RockPaperScissorsPage({Key? key}) : super(key: key);
 
-  Widget _handSelectButton(void Function()? onPressed, String text) {
+  @override
+  State<RockPaperScissorsPage> createState() => _RockPaperScissorsPageState();
+}
+
+class _RockPaperScissorsPageState extends State<RockPaperScissorsPage> {
+  String _displayTitleText = 'じゃんけん…';
+  String _displayCPUText = '相手「」';
+
+  String _handSelectText(HandSelect handSelect) {
+    switch (handSelect) {
+      case HandSelect.rock:
+        return 'グー';
+      case HandSelect.paper:
+        return 'パー';
+      case HandSelect.scissors:
+        return 'チョキ';
+      default:
+        return '';
+    }
+  }
+
+  void _onTapHandSelectButton(HandSelect playerHandSelect) {
+    HandSelect cpuHandSelect = HandSelect.rock;
+    setState(() {
+      _displayCPUText = _handSelectText(cpuHandSelect);
+    });
+    final bool win = (playerHandSelect == HandSelect.rock &&
+            cpuHandSelect == HandSelect.scissors) ||
+        (playerHandSelect == HandSelect.paper &&
+            cpuHandSelect == HandSelect.rock) ||
+        (playerHandSelect == HandSelect.scissors &&
+            cpuHandSelect == HandSelect.paper);
+    if (playerHandSelect == cpuHandSelect) {
+      // あいこ
+      setState(() {
+        _displayTitleText = 'あいこ…';
+      });
+    } else if (win) {
+      // 勝ち
+      setState(() {
+        _displayTitleText = '勝ち！';
+      });
+    } else {
+      // 負け
+      setState(() {
+        _displayTitleText = '負け';
+      });
+    }
+  }
+
+  Widget _handSelectButton(HandSelect handSelect) {
+    final String buttonText = _handSelectText(handSelect);
     return Expanded(
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 8.0),
         height: 64.0,
         child: TextButton(
-          onPressed: onPressed,
+          onPressed: () => _onTapHandSelectButton(handSelect),
           style: TextButton.styleFrom(
             primary: Colors.white,
             backgroundColor: Colors.black,
           ),
-          child: Text(text),
+          child: Text(buttonText),
         ),
       ),
     );
@@ -29,17 +86,20 @@ class RockPaperScissorsPage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Text(
-                'じゃんけん…',
-                style: TextStyle(fontSize: 32.0),
+              Text(
+                _displayTitleText,
+                style: const TextStyle(fontSize: 32.0),
+              ),
+              Text(
+                '相手「$_displayCPUText」',
+                style: const TextStyle(fontSize: 32.0),
               ),
               Container(
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
-                    _handSelectButton(() {}, 'グー'),
-                    _handSelectButton(() {}, 'チョキ'),
-                    _handSelectButton(() {}, 'パー'),
+                    for (var handSelect in HandSelect.values)
+                      _handSelectButton(handSelect)
                   ],
                 ),
               ),
