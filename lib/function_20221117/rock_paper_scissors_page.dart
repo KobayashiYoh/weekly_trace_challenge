c359d7e7eea09129c05cb1c7d0900c3b922a9f60
KobayashiYoh <y.kobayashi@4nonome.com>
2022-11-17 11:59:47 +0900

    基本的なUIを作成

--- lib/function_20221117/rock_paper_scissors_page.dart
+++ lib/function_20221117/rock_paper_scissors_page.dart
@@ -3,8 +3,50 @@ import 'package:flutter/material.dart';
 class RockPaperScissorsPage extends StatelessWidget {
   const RockPaperScissorsPage({Key? key}) : super(key: key);
 
+  Widget _handSelectButton(void Function()? onPressed, String text) {
+    return Expanded(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        height: 64.0,
+        child: TextButton(
+          onPressed: onPressed,
+          style: TextButton.styleFrom(
+            primary: Colors.white,
+            backgroundColor: Colors.black,
+          ),
+          child: Text(text),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: const EdgeInsets.symmetric(vertical: 64.0, horizontal: 16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                'じゃんけん…',
+                style: TextStyle(fontSize: 32.0),
+              ),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    _handSelectButton(() {}, 'グー'),
+                    _handSelectButton(() {}, 'チョキ'),
+                    _handSelectButton(() {}, 'パー'),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
